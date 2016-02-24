1f277d7075ead95f60ae600f5fb324f334fb62c2
dev <parksungho86@gmail.com>
2016-02-24 10:21:11 +0900

    ngClickOther

--- angular-essential.js
+++ angular-essential.js
@@ -22,6 +22,45 @@ angular.module('angular.essential', [])
             });
         };
     })
+    .factory('ngClickOtherService', function ($document) {
+        var tracker = [];
+        return function ($scope, expr) {
+            var i, t, len;
+            for (i = 0, len = tracker.length; i < len; i++) {
+                t = tracker[i];
+                if (t.expr === expr && t.scope === $scope) {
+                    return t;
+                }
+            }
+            var handler = function () {
+                $scope.$apply(expr);
+            };
+            $document.on('click', handler);
+            $scope.$on('$destroy', function () {
+                $document.off('click', handler);
+            });
+            t = {scope: $scope, expr: expr};
+            tracker.push(t);
+            return t;
+        };
+    })
+    .directive('ngClickOther', function ($document, ngClickOtherService) {
+        return {
+            restrict: 'A',
+            link: function (scope, elem, attr, ctrl) {
+                var handler = function (e) {
+                    e.stopPropagation();
+                };
+                elem.on('click', handler);
+
+                scope.$on('$destroy', function () {
+                    elem.off('click', handler);
+                });
+
+                clickAnywhereButHereService(scope, attr.clickAnywhereButHere);
+            }
+        };
+    })
     .service('$hangul', function () {
         this.getERO = function (word) {
             var last = word.charAt(word.length - 1);
