cdb8c22f39b8a16e52ee2f5c79c3965c374cba44
dev <parksungho86@gmail.com>
2016-02-28 14:59:51 +0900

    number only add

--- angular-essential.js
+++ angular-essential.js
@@ -22,6 +22,27 @@ angular.module('angular.essential', [])
             });
         };
     })
+    .directive('numberOnly', function () {
+        return {
+            require: 'ngModel',
+            link: function (scope, element, attr, ngModelCtrl) {
+                function fromUser(text) {
+                    if (text) {
+                        var transformedInput = text.replace(/[^0-9]/g, '');
+
+                        if (transformedInput !== text) {
+                            ngModelCtrl.$setViewValue(transformedInput);
+                            ngModelCtrl.$render();
+                        }
+                        return transformedInput;
+                    }
+                    return undefined;
+                }
+
+                ngModelCtrl.$parsers.push(fromUser);
+            }
+        };
+    })
     .factory('ngClickOtherService', function ($document) {
         var tracker = [];
         return function ($scope, expr, elem) {
