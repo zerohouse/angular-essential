48732d54beea8d9a45405ddc4f274bd5c296bbe3
dev <parksungho86@gmail.com>
2016-05-17 23:15:21 +0900

    business no added

--- angular-essential.js
+++ angular-essential.js
@@ -277,5 +277,55 @@ angular.module('angular.essential', [])
                 return value;
             }
         };
+    })
+    .directive('businessNo', function ($filter, $browser) {
+        return {
+            require: 'ngModel',
+            link: function ($scope, $element, $attrs, ngModelCtrl) {
+                var listener = function () {
+                    var value = $element.val().replace(/[^0-9]/g, '');
+                    $element.val($filter('businessNo')(value, false));
+                };
+
+                // This runs when we update the text field
+                ngModelCtrl.$parsers.push(function (viewValue) {
+                    return viewValue.replace(/[^0-9]/g, '');
+                });
+
+                // This runs when the model gets updated on the scope directly and keeps our view in sync
+                ngModelCtrl.$render = function () {
+                    $element.val($filter('businessNo')(ngModelCtrl.$viewValue, false));
+                };
+
+                $element.bind('change', listener);
+                $element.bind('keydown', function (event) {
+                    var key = event.keyCode;
+                    // If the keys include the CTRL, SHIFT, ALT, or META keys, or the arrow keys, do nothing.
+                    // This lets us support copy and paste too
+                    if (key == 91 || (15 < key && key < 19) || (37 <= key && key <= 40)) {
+                        return;
+                    }
+                    $browser.defer(listener); // Have to do this or changes don't get picked up properly
+                });
+
+                $element.bind('paste cut', function () {
+                    $browser.defer(listener);
+                });
+            }
+        };
+    })
+    .filter('businessNo', function () {
+        return function (card) {
+            if (!card) {
+                return '';
+            }
+            var value = card.toString().trim().replace(/^\+/, '');
+            if (value.match(/[^0-9]/)) {
+                return card;
+            }
+            if (value) {
+                return value.slice(0, 3) + '-' + value.slice(3, 5) + '-' + value.slice(5, 10);
+            }
+        };
     });
 
